8e63b6f1254cc6bf343ac55d0f335f2ef1451ee3
taiwo17 <shoboadefowope2007@gmail.com>
2022-11-30 16:27:12 -0800

    Update the morgan node package

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 import express from 'express'
 import http from 'http'
+const morgan = require('morgan')
 
 const hostname = 'localhost'
 const port = 4000
 
 const app = express()
+app.use(morgan('dev'))
+
+app.use(express.static(__dirname + '/public'))
 
 app.use((req, res, next) => {
-  console.log(req.headers)
+  //   console.log(req.headers)
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
   res.send(
